0b03f9034e7fb02583ccfc4ceaf35a7f3e13c891
刘正伟 <liuzhengwei@100tal.com>
2019-03-11 18:09:04 +0800

    no dir bug fix

--- src/Middleware/RunningTimeMiddleware.php
+++ src/Middleware/RunningTimeMiddleware.php
@@ -17,6 +17,11 @@ class RunningTimeMiddleware
     {
         $response =  $next($request);
 
+        $path = storage_path('logs/runningtime/');
+        if (!is_dir($path)) {
+            mkdir($path);
+        }
+
         if (!app()->runningInConsole()) {
             $log = [
                 'time' => round(microtime(true) - LARAVEL_START, 2),
@@ -28,7 +33,7 @@ class RunningTimeMiddleware
             $logJson = json_encode($log, JSON_UNESCAPED_UNICODE);
             $logFile = date("Y-m-d") . '.log';
 
-            file_put_contents(storage_path('logs/runningtime/' . $logFile), $logJson . "\n", FILE_APPEND);
+            file_put_contents($path . '/' . $logFile, $logJson . "\n", FILE_APPEND);
         }
 
         return $response;
