01aa7f23dcb18b04dc45ed8f699c6a829ccacc57
刘正伟 <liuzhengwei@100tal.com>
2019-03-11 15:15:46 +0800

    命令行日志分析

--- src/Commands/TimeCommand.php
+++ src/Commands/TimeCommand.php
@@ -14,7 +14,7 @@ class TimeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'execution-time {--startTime?}';
+    protected $signature = 'execution-time {line=10 : 最多展示多少行} {--startTime? : 开始时间} {--endTime? : 结束时间}';
 
     /**
      * The console command description.
@@ -22,6 +22,16 @@ class TimeCommand extends Command
      * @var string
      */
     protected $description = '统计页面运行时间';
+
+    public $line;
+
+    /** @var \DateTime $start */
+    public $start;
+
+    /** @var \DateTime $end */
+    public $end;
+
+    protected $logPath;
 
     /**
      * Create a new command instance.
@@ -31,6 +41,12 @@ class TimeCommand extends Command
     public function __construct()
     {
         parent::__construct();
+
+        $this->logPath = storage_path('logs/ExecutionTime');
+
+        $this->line = $this->argument('line');
+        $this->start = $this->option('startTime') ?? (new \DateTime())->modify('-7 days');
+        $this->end = $this->option('endTime') ?? (new \DateTime());
     }
 
     /**
@@ -41,5 +57,66 @@ class TimeCommand extends Command
     public function handle()
     {
 
+        $this->longestTime();
+    }
+
+    public function longestTime()
+    {
+        $logs = $this->getLogFiles();
+
+        $urlTimes = $times = [];
+
+        foreach ($logs as $log) {
+            $log = json_decode(trim($log), true);
+            $urlTimes[$log['url']][] = $log['time'];
+        }
+
+
+        foreach ($urlTimes as $url => &$time) {
+            $cnt = count($time);
+            $max = max($time);
+            $min = min($time);
+            $average = round(array_sum($time) / $cnt);
+
+            $time = [
+                'url' => $url,
+                'average' => $average,
+                'max' => $max,
+                'min' => $min,
+            ];
+
+            $times[$url] = $average;
+        }
+
+        rsort($times);
+        $times = array_slice($times, 0, $this->line);
+
+        $this->table(['url', 'average', 'max', 'min'], $times);
+    }
+
+    /**
+     * 获取所有符合条件的日志内容
+     *
+     * @return array
+     */
+    protected function getLogFiles()
+    {
+        $files = [];
+        $days = $this->end->diff($this->start)->format('%a');
+
+        for ($n = 0; $n < $days; $n++) {
+            $files[] = $this->logPath . '/' . $this->start->format('Y-m-d') . '.log';
+            $this->start->modify('+1 days');
+        }
+
+        $contents = [];
+
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                $contents = array_merge($contents, file($file));
+            }
+        }
+
+        return $contents;
     }
 }
--- src/Middleware/ExecutionTimeMiddleware.php
+++ src/Middleware/ExecutionTimeMiddleware.php
@@ -20,14 +20,14 @@ class ExecutionTimeMiddleware
         if (!app()->runningInConsole()) {
             $log = [
                 'time' => round(microtime(true) - LARAVEL_START, 2),
-                'uri' => $request->fullUrl(),
+                'uri' => $request->url(),
                 'params' => $request->all(),
             ];
 
             $logJson = json_encode($log, JSON_UNESCAPED_UNICODE);
             $logFile = date("Y-m-d") . '.log';
 
-            file_put_contents(storage_path('logs/' . $logFile), $logJson . "\n", FILE_APPEND);
+            file_put_contents(storage_path('logs/ExecutionTime/' . $logFile), $logJson . "\n", FILE_APPEND);
         }
 
         return $response;
